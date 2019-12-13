f6efc45ae05bcc033cc2a9660e5b2e172d2a168e
yann <yang.wang@ipiaoniu.com>
2019-12-13 17:28:42 +0800

    fix

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,37 +1,11 @@
 import linkedlist.SkipList;
+import searchsort.ContainerWithMostWater;
 
 public class Test {
 
     public static void main(String args[]){
-        SkipList skipList = new SkipList();
-        skipList.add(1);
-        skipList.add(2);
-        skipList.add(3);
-        skipList.search(0);
-        skipList.add(4);
-        skipList.add(1);
-        skipList.add(1);
-        skipList.add(1);
-        skipList.add(1);
-        skipList.add(1);
-        skipList.erase(1);
-        skipList.erase(1);
-        skipList.erase(1);
-        skipList.erase(1);
-        skipList.erase(1);
-        skipList.erase(1);
-        skipList.erase(1);
-        skipList.add(1);
-        skipList.add(1);
-        skipList.add(1);
-        skipList.add(1);
-        skipList.add(1);
-        skipList.add(1);
-        skipList.search(1);
-        skipList.erase(0);
-        skipList.erase(1);
-        skipList.search(1);
-        skipList.erase(1);
-        skipList.search(1);
+        ContainerWithMostWater water = new ContainerWithMostWater();
+        int[] a = {4,4,2,11,0,11,5,11,13,8};
+        water.maxArea(a);
     }
 }
